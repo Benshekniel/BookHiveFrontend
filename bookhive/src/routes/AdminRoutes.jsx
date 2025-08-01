788dfd44595d9361abb16681b9ca067cca77ac86
S.Mathulan <158765049+Mathulan29@users.noreply.github.com>
2025-08-01 23:12:22 +0530

    Update AdminRoutes.jsx

--- bookhive/src/routes/AdminRoutes.jsx
+++ bookhive/src/routes/AdminRoutes.jsx
@@ -5,6 +5,7 @@ import Dashboard from '../pages/admin/Dashboard';
 import Analytics from '../pages/admin/Analytics';
 import ModeratorManagement from '../pages/admin/ModeratorManagement';
 import Messages from '../pages/admin/Messages';
+import PaymentAnalytics from '../pages/admin/PaymentAnalytics';
 
 
 const AdminRoutes = () => (
@@ -15,6 +16,7 @@ const AdminRoutes = () => (
       <Route path="analytics" element={<Analytics />} />
       <Route path="moderator" element={<ModeratorManagement />} />
       <Route path="messages" element={<Messages />} />
+      <Route path="payment" element={<PaymentAnalytics />} />
       
     </Routes>
   </DashboardLayout>
